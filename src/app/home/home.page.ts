36f800048bc7d8cedfa5bc2e48d1031661374a48
Simon Jose Gil Romero <sgil@ADADOM.LOCAL>
2024-04-09 12:01:40 -0500

    new code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Pipe, PipeTransform } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private menuCtrl: MenuController, private sanitizer: DomSanitizer) {}
+
+  app: any;
+  selectedApp = 2;
+
+  changeApp(on:number) {
+    this.selectedApp = on;
+    console.log('frame',  this.selectedApp);
+  }
+
+  microfrontends = [
+    { id: 1, label: 'Desplegar microfrontend 1', url: 'http://localhost:4200/' },
+    { id: 2, label: 'Desplegar microfrontend 2', url: 'http://localhost:64383/' },
+    { id: 3, label: 'Desplegar microfrontend 3', url: 'http://localhost:56936/' }
+  ];
 
 }
+
