bdcd93c55fadba61efc5ebb8d8e922902f8fbc81
Simon Jose Gil Romero <sgil@ADADOM.LOCAL>
2024-04-10 14:19:27 -0500

    correcciones antes de componetizar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,46 +12,46 @@ export class HomePage implements OnInit{
 
 @ViewChild('miFrame') miFrame!: ElementRef;
 
-src: string = '';
-paginaActual: string = '';
-selectedApp: any;
-idAct: any;
-clickApp: number = 0;
-microfrontends = [
-  { id: 1, name: 'Desplegar microfrontend 1', url: 'http://localhost:4200/', classStyle: 'width: 100%; height: 100vh; border: none;' },
-  { id: 2, name: 'Desplegar microfrontend 2', url: 'http://localhost:64383/', classStyle: 'width: 100%; height: 100vh; border: none;' },
-  { id: 3, name: 'Desplegar microfrontend 3', url: 'http://localhost:52612/', classStyle: 'width: 100%; height: 140vh; border: none;' }
-];
+  src: string = '';
+  paginaActual: string = '';
+  selectedApp: any;
+  idAct: any;
+  clickApp: number = 0;
+  microfrontends = [
+    { id: 1, name: 'Desplegar microfrontend 1', url: 'http://localhost:4200/', classStyle: 'width: 100%; height: 100vh; border: none;' },
+    { id: 2, name: 'Desplegar microfrontend 2', url: 'http://localhost:64383/', classStyle: 'width: 100%; height: 100vh; border: none;' },
+    { id: 3, name: 'Desplegar microfrontend 3', url: 'http://localhost:52612/', classStyle: 'width: 100%; height: 140vh; border: none;' }
+  ];
 
 
-constructor(private menuCtrl: MenuController, private activatedRoute: ActivatedRoute) {}
+  constructor(private menuCtrl: MenuController, private activatedRoute: ActivatedRoute) {}
 
-ngOnInit() {
-  this.clickApp = 0;
-  this.getUrl();
-  window.addEventListener('message', this.handleMessage.bind(this), false);
-}
-
-getUrl() {
-  this.idAct = localStorage.getItem('idAct') || '';
-  this.selectedApp = parseInt(this.idAct);
-  this.paginaActual = localStorage.getItem('paginaActual') || '';
-}
-
-handleMessage(event: MessageEvent) {
-  const messageData = event.data;
-  if (messageData && messageData.url) {
-    this.paginaActual = messageData.url;
-    localStorage.setItem('paginaActual', this.paginaActual);
+  ngOnInit() {
+    this.clickApp = 0;
+    this.getUrl();
+    window.addEventListener('message', this.handleMessage.bind(this), false);
   }
-}
 
-changeApp(on:number) {
-  this.clickApp = on; 
-  this.selectedApp = on;
-  localStorage.setItem('idAct', this.selectedApp);
-  console.log('frame',  this.selectedApp);
-}
+  getUrl() {
+    this.idAct = localStorage.getItem('idAct') || '';
+    this.selectedApp = parseInt(this.idAct);
+    this.paginaActual = localStorage.getItem('paginaActual') || '';
+  }
+
+  handleMessage(event: MessageEvent) {
+    const messageData = event.data;
+    if (messageData && messageData.url) {
+      this.paginaActual = messageData.url;
+      localStorage.setItem('paginaActual', this.paginaActual);
+    }
+  }
+
+  changeApp(on:number) {
+    this.clickApp = on; 
+    this.selectedApp = on;
+    localStorage.setItem('idAct', this.selectedApp);
+    console.log('frame',  this.selectedApp);
+  }
 
 }
 
